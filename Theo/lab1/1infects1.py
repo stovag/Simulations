af6c5dd13876e7c6035c5359c87c17da27155cb7
Theocharis Panagiotis Charalampidis <runtheorun3@gmail.com>
2022-02-07 14:41:56 +0200

    contributions

---
+++ Theo/lab1/1infects1.py
@@ -0,0 +1,36 @@
+from random import randint
+import matplotlib.pyplot as plt
+import numpy as np
+
+n = 1000
+nodes = [0] * n
+infectionDone = False
+currentInfected = 1
+infectedPerLoop = 0
+infectionRate = []
+curInf = []
+attempts = []
+x=1
+
+nodes[randint(0,n-1)] = 1
+
+while not infectionDone:
+    for i in range(n):
+        if nodes[i] == 1:
+            target = randint(0,n-1)
+            if nodes[target] == 0:
+                nodes[target] = 1
+                infectedPerLoop+=1
+        currentInfected += infectedPerLoop
+        curInf.append(currentInfected)
+        infectedPerLoop = 0
+    infectionRate.append (currentInfected)
+    attempts.append(x)
+    x+=1
+    if currentInfected == n:infectionDone = True 
+
+xpoints = np.array(attempts)
+ypoints = np.array(infectionRate)
+plt.plot(xpoints, ypoints)
+plt.savefig("lab1/infectionrate.jpg")
+plt.show()
---
+++ Theo/lab3/antivirus.py
@@ -0,0 +1,49 @@
+import random
+from random import randint
+import matplotlib.pyplot as plt
+import numpy as np
+
+class NodeProps:
+    def __init__(self,status, infection_lambda):
+        self.status = status # 0 = healthy, 1 = infected, 2 = recovered
+        self.infection_lambda = infection_lambda # time until node recovers
+
+    
+n = 1000
+nodesList = [0] * n
+for i in range(0,n):nodesList[i] = NodeProps(0,0)
+infectionDone = False
+currentInfected = 1
+infectedPerLoop = 0
+infectionRate = []
+curInf = []
+attempts = []
+x=1
+nodesList[5] = NodeProps(1,0)
+while not infectionDone:
+    for i in range(n):
+        if nodesList[i].status == 1:
+            target = randint(0,n-1)
+            if nodesList[target].status == 0:
+                nodesList[target].status = 1
+                infectedPerLoop+=1
+    for i in range(n):
+        if nodesList[i].status == 1:
+            nodesList[i].infection_lambda += 1
+    for i in range(n):    
+        if nodesList[i].infection_lambda == 10:
+            nodesList[i].status = 2
+
+    currentInfected += infectedPerLoop
+    curInf.append(currentInfected)
+    infectedPerLoop = 0
+    infectionRate.append (currentInfected)
+    attempts.append(x)
+    x+=1
+    if currentInfected == n:infectionDone = True 
+
+xpoints = np.array(attempts)
+ypoints = np.array(infectionRate)
+plt.plot(xpoints, ypoints)
+plt.savefig("lab3/infectionrate.jpg")
+plt.show()
